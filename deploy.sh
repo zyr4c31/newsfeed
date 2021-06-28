471274d64759622b2ca22b2e5f1771759dcb8952
zyr4c31 <zyr4c31@gmail.com>
2021-06-28 15:02:27 +0800

    github pages deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 set -e
 
 git init
-git branch master
 git add -A
 git commit -m 'github pages deployment'
 git push -f git@github.com:zyr4c31/newsfeed.git master
@@ -14,6 +13,6 @@ cd dist
 git init
 git add -A
 git commit -m 'github pages deployment'
-git push -f git@github.com:zyr4c31/newsfeed.git master:gh
+git push -f git@github.com:zyr4c31/newsfeed.git master:gh-pages
 
 cd -
